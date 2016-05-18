72f344fb8bdba999d681e22b5f5f77c48cf0925d
Preetesh Jain <preeteshjain17@yahoo.in>
2016-05-19 00:52:39 +0530

    Sanitize JS files

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,21 +1,3 @@
 $(function() {
-    $('select').material_select();
-    $.ajax({
-        url:'http://hcetnotix.azurewebsites.net/process/getBranches',
-        type:'POST',
-        dataType: 'json',
-        success: function( json ) {
-            $ip = $('#studentRegForm_branchSelect');
-            $.each(json, function(i, value) {
-                $('#studentRegForm_branchSelect').append('<option value=' + value.branch + '>' + value.branch + '</option>');
-            });
-            $ip.prop("disabled", false);
-            $ip.material_select();
-        }
-    });
-
-    $('#profilePic').on('click', function() {
-        $('#tipModal').openModal();
-        $('#picturePicker').show().focus().click().hide();
-    });
+    
 });
