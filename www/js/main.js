e25a9fd74057b324eb24caf061a2b3686a9398ad
Preetesh Jain <preeteshjain17@yahoo.in>
2016-05-20 01:49:31 +0530

    Add push.on notification event

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
-    
+    var push = PushNotification.init({
+        android: {
+            senderID: "72429857274"
+        }
+    });
+    push.on('notification', function(data) {
+        alert("New Notification! " + data.message + " " + data.message);
+    });
 });
